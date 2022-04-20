d6f66adcdcb083d0905460ba1d0fa28e9e4c0a5d
kism6795 <76738454+kism6795@users.noreply.github.com>
2022-04-19 21:10:32 -0600

    Performed non-parametric statistics for hypotheses 1-3 & uploaded plots & new data (22 subjects)

--- processing/analysis.R
+++ processing/analysis.R
@@ -2,8 +2,7 @@
 data1 <- read.csv("processedData2.csv")
 nSubs = length(data1$Timestamp);
 
-# ANOVA Table for H1!
-
+################################## H1 ######################################
 # h1: Still to Do!
 # alpha = slow/easy
 # beta = fast/easy
@@ -13,8 +12,9 @@ wkldData = c(data1$WorkloadA, data1$WorkloadB, data1$WorkloadC, data1$WorkloadD)
 tetrisDiff = c(rep(0,nSubs),rep(0,nSubs),rep(1,nSubs),rep(1,nSubs))
 wordSpd = c(rep(0,nSubs),rep(1,nSubs),rep(0,nSubs),rep(1,nSubs))
 condition = c(rep(1,nSubs),rep(2,nSubs),rep(3,nSubs),rep(4,nSubs))
-h1 = aov(wkldData~tetrisDiff+wordSpd+tetrisDiff:wordSpd)
-summary(h1)
+condition <- factor(condition)
+levels(condition)<-c("Alpha","Beta","Gamma","Delta")
+
 h1a = aov(wkldData~tetrisDiff+wordSpd)
 summary(h1a)
 
@@ -27,13 +27,24 @@ interaction.plot(x.factor = wordSpd, trace.factor = tetrisDiff, response = wkldD
                  lwd = 2, #line width
                  trace.label = "Game Difficulty")
 
-interaction.plot(x.factor = tetrisDiff, trace.factor = wordSpd, response = wkldData, fun = median,   ylab = "Workload",
-                 xlab = "Game Difficulty",
-                 col = c("red", "blue"),
-                 lty = 1, #line type
-                 lwd = 2, #line width
-                 trace.label = "Word Presentation Rate")
 
+# hypothesis 1: 2-factor ANOVA
+h1a <- aov(wkldData~factor(tetrisDiff) + factor(wordSpd) + factor(tetrisDiff):factor(wordSpd) +Error(factor(subjNum)))
+summary(h1a)
+
+# h1: test for normality
+hist(wkldData, main="H1) Histogram of Workload",
+     xlab="Workload Rating", ylab="Frequency")
+shapiro.test(wkldData)
+
+# to present! h1: non-parametric, friedmans anova with condition
+subjNum = factor(rep(1:nSubs,4))
+h1f = friedman.test(wkldData,condition,subjNum)
+summary(h1f)
+boxplot(wkldData~condition, main="H1) Workload by Condition",
+        xlab="Condition", ylab="Workload Rating")
+
+################################## H2 ######################################
 # h2: workload vs recall linear regression --> not sure this is really allowed w rep measures
 rcllData = 100*c(data1$recallA, data1$recallB, data1$recallC, data1$recallD)
 wkldData = c(data1$WorkloadA, data1$WorkloadB, data1$WorkloadC, data1$WorkloadD)
@@ -43,7 +54,17 @@ plot(wkldData,rcllData, main="H2) Recall vs Workload",
 abline(h2)
 boxplot(rcllData~condition)
 
+# h2: test for normality
+hist(rcllData, main="H1) Histogram of Recall Percentage",
+     xlab="Recall Percentage", ylab="Frequency")
+shapiro.test(rcllData)
 
+#h2: non-parametric, friedmans anova with condition
+h2f = friedman.test(rcllData,condition,subjNum)
+boxplot(rcllData~condition, main="H2) Recall % by Condition",
+        xlab="Condition", ylab="Recall %")
+
+################################## H3 ######################################
 # h3: workload vs precision linear regression
 # should be ANOVA instead
 prcsnData = 100*c(data1$precisionA, data1$precisionB, data1$precisionC, data1$precisionD)
@@ -53,24 +74,49 @@ plot(wkldData,prcsnData, main="H3) Precision vs Workload",
 abline(h3)
 boxplot(prcsnData~condition)
 
+# h3: test for normality
+hist(prcsnData, main="H3) Histogram of Precision Percentage",
+     xlab="Precision Percentage", ylab="Frequency")
+shapiro.test(prcsnData)
+
+#h3: non-parametric, friedmans anova with condition
+h3f = friedman.test(prcsnData,condition,subjNum)
+boxplot(prcsnData~condition, main="H3) Precision % by Condition",
+        xlab="Condition", ylab="Precision %")
+
+################################## H0 ######################################
+# not kosher stats but interesting that precision is significant and recall is not
+# correlating precision and workload
+# correlating recall and workload
+plot(wkldData,prcsnData)
+plot(wkldData,rcllData)
+corr <- cor.test(prcsnData, wkldData, method = 'spearman')
+corr <- cor.test(rcllData, wkldData, method = 'spearman')
+
+################################## H4 ######################################
 # h4: tetris experience vs workload
 wkldDataAvg = integer(nSubs)
 for (i in 1:nSubs) {
   wkldDataAvg[i] = mean(data1$WorkloadA[i], data1$WorkloadB[i], data1$WorkloadC[i], data1$WorkloadD[i])
 }
-r4 = cor(wkldDataAvg,data1$TetrisXP)
-h4 = lm(wkldDataAvg~data1$TetrisXP)
-plot(data1$TetrisXP,wkldDataAvg, main="H4a) Workload vs. Tetris Experience",
-     xlab="Self-Rated Tetris Experience", ylab="Reported Workload [MBS]")
-abline(h4)
+shapiro.test(wkldDataAvg)
 
-# h4a: high workload experience vs workload
+# h4a: tetris experience vs workload
 r4a = cor(wkldDataAvg,data1$TetrisXP)
 h4a = lm(wkldDataAvg~data1$TetrisXP)
-plot(data1$WorkloadXP,wkldDataAvg, main="H4b) Workload vs. High-Workload Experience",
-     xlab="Self-Rated High-Workload Experience", ylab="Reported Workload [MBS]")
+plot(data1$TetrisXP,wkldDataAvg, main="H4a) Workload vs. Tetris Experience",
+     xlab="Self-Rated Tetris Experience", ylab="Reported Workload [MBS]")
 abline(h4a)
 
+
+# h4b: high workload experience vs workload
+r4b = cor(wkldDataAvg,data1$WorkloadXP)
+h4b = lm(wkldDataAvg~data1$WorkloadXP)
+plot(data1$WorkloadXP,wkldDataAvg, main="H4b) Workload vs. High-Workload Experience",
+     xlab="Self-Rated High-Workload Experience", ylab="Reported Workload [MBS]")
+abline(h4b)
+
+################################## H5 ######################################
 # h5: game scores and recall % --> also not sure these two are entirely legal
 scoreData = c(data1$ScoreA, data1$ScoreB, data1$ScoreC, data1$ScoreD)
 r5 = cor(scoreData,rcllData)
